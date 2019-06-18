ad37fa243128f4fc066e76a691ddcae9359db45c
freddie-devops <51332673+freddie-devops@users.noreply.github.com>
2019-06-18 12:10:22 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+
 if curl -s --head  --request GET http://a9fc84018913511e990920682ffff8bc-2121391132.us-east-2.elb.amazonaws.com:8080 | grep "200 OK" > /dev/null; then 
    echo "Spring Boot App is UP"
 else
