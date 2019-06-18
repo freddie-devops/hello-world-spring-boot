b1e4639bdd3b25d8dc1a85cc6b720c5ae7397dd8
freddie-devops <51332673+freddie-devops@users.noreply.github.com>
2019-06-18 12:44:11 -0400

    test

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "<html><center><font face='arial' size='10' color='#8ebf42'>Welcome to FreddieMac Spinnaker POC <br> by Pricing Devops Team <br> </font> <font face='verdana' color='black'>This is a simple Spring Boot Application <br> Successful Response...:)</font></center></html>";
+        return "<html><center><font face='arial' size='10' color='#8ebf42'>Welcome to FreddieMac Spinnaker POC <br> by Pricing Team <br> </font> <font face='verdana' color='black'>This is a simple Spring Boot Application <br> Successful Response...:)</font></center></html>";
     }
 
     public static void main(String[] args) throws Exception {
