bc1a9c3a7a46f0c20f1c6bcf57f7a5ccd52612c4
freddie-devops <51332673+freddie-devops@users.noreply.github.com>
2019-06-18 11:52:00 -0400

    test commit

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-curl http://a9fc84018913511e990920682ffff8bc-2121391132.us-east-2.elb.amazonaws.com:8080 
+if curl -s --head  --request GET http://a9fc84018913511e990920682ffff8bc-2121391132.us-east-2.elb.amazonaws.com:8080 | grep "200 OK" > /dev/null; then 
+   echo "Spring Boot App is UP"
+else
+   echo "Spring Boot App  is DOWN"
+fi
